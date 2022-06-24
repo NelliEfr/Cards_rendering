9baabdf86295049f99d4958586ae15bec8436e7c
Nelli Efremyan <n.efremyan@gmail.com>
2022-06-24 14:17:28 +0400

    form started, array collects data

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-console.log('hello');
+// Создайте форму сбора данных работников с тремя полями: Имя, Фамилия, Возраст. Поля ввода должны включать "текст-подсказку". Форма должна собирать полученные данные в массив, 
+// а затем формировать карточки работников с полученной информацией.
+
+const formElem = document.querySelector('.workers_form');
+let workersArr = [];
+
+formElem.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const {firstname, lastname, age} = event.target; //this
+  workersArr.push({
+    firstname: firstname.value,
+    lastname: lastname.value,
+    age: age.value,
+  })
+  firstname.value = '';
+  lastname.value = '';
+  age.value = '';
+  console.log(workersArr);
+})
